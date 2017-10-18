1b7d6a498e688df59f0c39e99cdbf0c26b375706
erkutbas <ebas1@binghamton.edu>
2017-10-18 13:42:34 +0300

    update
    update

--- remzi/remzi/ViewController.swift
+++ remzi/remzi/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("erkut")
     }
 
     override func didReceiveMemoryWarning() {
