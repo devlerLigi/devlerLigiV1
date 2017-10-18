ff80bb910846cf77d9aa91bc7a4a3884739de1c7
ugurgog <32577192+ugurgog@users.noreply.github.com>
2017-10-18 18:53:58 +0300

    ugurv1

---
+++ githubdemougur/app/src/main/java/com/example/mac/githubdemougur/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mac.githubdemougur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
